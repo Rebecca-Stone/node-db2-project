f241941e5d66ff4c8bdb476ac076701fa22a8b61
Rebecca-Stone <primstone16@gmail.com>
2022-04-19 15:38:10 -0500

    setting project up and added directions within code for myself. will remove later

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,12 @@
 // DO YOUR MAGIC
+
+// ### Write a Cars API
+
+// - Write CR (of CRUD) for the `cars` resource, using the middleware and model functions described above wherever appropriate inside `api/cars/cars-router.js` :
+
+//   - `[GET] /api/cars` returns an array of cars sorted by id (or an empty array if there aren't any).
+//   - `[GET] /api/cars/:id` returns a car by the given id.
+//   - `[POST] /api/cars` returns the created car.
+
+// - Manually test your endpoints with a REST client like `Insomnia` or `Postman` to check they are working as expected.
+// - Test your endpoints automatically by running `npm test`.
